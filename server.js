5a24ef58b4bac2c960c652e78cf2e831c8896925
richardbsam <richardbonifacesam@gmail.com>
2025-03-11 10:52:20 +0100

    firstcommit

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 // express web server
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use('/', require('./routes'))
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}`)
+})
