cf338cb9ae1fa9a1f9d1902396cc3863b0b3286b
richardbsam <richardbonifacesam@gmail.com>
2025-03-12 16:16:23 +0100

    single updated

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,11 +1,32 @@
 const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
 
-const getData = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('user').find();
-  result.toArray().then((lists) => {
+
+const getAll = async (req, res) => {
+  const result = await mongodb.getDb().db().collection('contacts').find();
+  result.toArray().then((contacts) => {
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]); // we just need the first one (the only one)
+    res.status(200).json(contacts);
   });
+
 };
 
-module.exports = { getData };
+
+const getSingle = async (req, res) => {
+  try {
+    const contactId = new ObjectId(req.params.id);
+    const contact = await mongodb.getDb().db().collection('contacts').findOne({ _id: contactId });
+
+    if (!contact) {
+      return res.status(404).json({ message: 'Contact not found' });
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(contact);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching contact', error });
+  }
+}
+
+
+module.exports = { getAll, getSingle };
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const router = express.Router();
 
 const contactsController = require('../controllers/contacts');
 
-const router = express.Router();
 
 // GET /feed/posts
-router.get('/', contactsController.getData);
-// localhost:3000/contacts/
+router.get('/', contactsController.getAll);
+router.get('/:id', contactsController.getSingle);
+
 module.exports = router;
