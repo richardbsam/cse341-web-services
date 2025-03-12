2b93f794ee9ace5deaa671024b2722ac8f69af78
richardbsam <richardbonifacesam@gmail.com>
2025-03-12 12:02:40 +0100

    project01

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,18 @@ routes.get('/john', jokes.displayJohn);
 routes.get('/richard', jokes.displayRichard);
 
 module.exports = routes;
+
+
+
+// new new
+
+const express = require('express');
+
+const professionalController = require('../controllers/professional');
+
+const router = express.Router();
+
+// GET /feed/posts
+router.get('/', professionalController.getData);
+// localhost:8080/professional/
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 // express web server
 const express = require('express');
+const mongodb = require('./db/connect');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+
 app.use('/', require('./routes'))
 
-app.listen(port, () => {
-    console.log(`Running on port ${port}`)
-})
+
+
+
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to Database and listening on ${port}`);
+  }
+});
