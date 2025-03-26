7945b3809da42f1a77d37b46a7192c9bf099c9d1
richardbsam <richardbonifacesam@gmail.com>
2025-03-26 17:00:04 +0100

    folder created for project01

--- server.js
+++ server.js
@@ -24,39 +24,3 @@ mongodb.initDb((err) => {
   }
 });
 
-
-
-
-/*
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongodb = require('./db/connect');
-
-const port = process.env.PORT || 3000;
-const app = express();
-
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
-  .use('/', require('./routes'));
-
-// Root Route
-const routes = require('./routes/index'); // Import your routes
-
-app.use('/', routes); // Use the routes correctly
-
-// Initialize DB and Start Server
-mongodb.initDb((err) => {
-  if (err) {
-    console.error('❌ Database connection failed:', err);
-  } else {
-    app.listen(port, () => {
-      console.log(`✅ Connected to Database and listening on port ${port}`);
-    });
-  }
-});
-
-*/
